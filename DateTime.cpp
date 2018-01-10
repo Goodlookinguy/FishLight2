b9f8c4f0362b45dfd1a060e91fd3d3c710f00760
Goodlookinguy <Goodlookinguy@nrgs.org>
2018-01-10 03:06:18 -0800

    Implemented DateTime::DayOfYear, DateTime::DaysInMonth, and DateTime::DayOfWeek

--- DateTime.cpp
+++ DateTime.cpp
@@ -4,7 +4,22 @@
 
 #include "DateTime.h"
 
-bool DateTime::IsLeapYear(uint32_t year)
+const int16_t DateTime::s_monthsInDays[] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+
+int16_t DateTime::daysFromCalendarStart(int16_t year, int8_t month, int8_t day)
+{
+	const uint16_t febFix{ month <= 2 };
+	
+	return 5 +
+		(year - 1700) * 365 +
+		(year - 1700 - febFix) / 4 -
+		(year - 1700 - febFix) / 100 +
+		(year - 1600 - febFix) / 400 +
+		s_monthsInDays[month - 1] +
+		(day - 1);
+}
+
+bool DateTime::IsLeapYear(int32_t year)
 {
 	return (year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0);
 }
@@ -26,6 +41,26 @@ String DateTime::Meridiem(uint8_t hour)
 	if (hour >= 12)
 		return "PM";
 	return "AM";
+}
+
+int16_t DateTime::DayOfYear(int16_t year, int8_t month, int8_t day)
+{
+	return s_monthsInDays[month - 1] + day + 1 * (IsLeapYear(year) && month > 2);
+}
+
+int16_t DateTime::DayOfWeek(int16_t year, int8_t month, int8_t day)
+{
+	return daysFromCalendarStart(year, month, day) % 7;
+}
+
+int16_t DateTime::DaysInMonth(int16_t year, int8_t month)
+{
+	if (month == 2)
+		return 28 + 1 * IsLeapYear(year);
+	if (month < 8 && (month % 2) == 1 ||
+		month > 7 && (month % 2) == 0)
+		return 31;
+	return 30;
 }
 
 
--- DateTime.h
+++ DateTime.h
@@ -12,13 +12,23 @@
 
 class DateTime
 {
-	static bool IsLeapYear(uint32_t year);
+private:
+	const static int16_t s_monthsInDays[];
+	static int16_t daysFromCalendarStart(int16_t year, int8_t month, int8_t day);
+
+public:
+	static bool IsLeapYear(int16_t year);
 
 	// Returns false for AM and true for PM
 	static bool TimeOfDay(uint8_t hour);
 
 	static String Meridiem(bool timeOfDay);
 	static String Meridiem(uint8_t hour);
+
+	static int16_t DayOfYear(int16_t year, int8_t month, int8_t day);
+	static int16_t DayOfWeek(int16_t year, int8_t month, int8_t day);
+
+	static int16_t DaysInMonth(int16_t year, int8_t month);
 };
 
 
